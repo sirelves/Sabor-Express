ed74e2fe2e11fa83d91279bcaca63ec26eae3ec2
Elves Santos <elvesdev@proton.me>
2025-03-11 21:36:20 -0300

    Exercicios do modulo "Manipulando Strings". exercicio_01.py

---
+++ Exercicios_Alura/exercicio_01.py
@@ -0,0 +1,27 @@
+'''
+
+A função print() desempenha um papel fundamental para imprimir texto e variáveis no console ou em outro local de saída. Ao longo das diferentes versões do Python, a sintaxe e os recursos dessa função evoluíram, oferecendo diversas maneiras de formatar e exibir mensagens.
+
+Para praticar essa função, criamos a seguir uma lista de atividades (não obrigatórias) focada em prática para melhorar ainda mais sua experiência de aprendizagem. Bora praticar então?
+
+Exercícios
+1 - Imprima a frase: Python na Escola de Programação da Alura.
+
+2 - Imprima a frase: Meu nome é {nome} e tenho {idade} anos em que nome e idade precisam ser valores armazenados em variáveis.
+
+3 - Imprima a palavra: ‘ALURA’ de modo que cada letra fique em uma linha, como mostrado a seguir:
+'''
+
+
+print('Python na Escola de Programação da Alura.\n')
+
+nome = 'elves'
+idade = 22
+
+print(f'Meu nome é {nome} e tenho {idade} anos\n')
+
+print('A')
+print('L')
+print('U')
+print('R')
+print('A')
