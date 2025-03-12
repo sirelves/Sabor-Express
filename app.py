c0998ddfd6cff22150c3e384b7fea59535e0f991
Elves Santos <elvesdev@proton.me>
2025-03-11 21:47:14 -0300

    Aprendi um pouco sobre estrutura de repetição

--- app.py
+++ app.py
@@ -15,7 +15,13 @@ print('4. Sair\n')
 
 opcao_escolhida = input('Escolha uma opção: ')
 
-print(f'Você escolheu a opção: {opcao_escolhida}')
+# 1print(f'Você escolheu a opção: {opcao_escolhida}')
 
-
-
+if opcao_escolhida == '1':
+    print("Cadastrar restaurante")
+elif opcao_escolhida == '2':
+    print('Listar restaurantes')
+elif opcao_escolhida == '3':
+    print('Ativar restaurante')
+else:
+    print('Saindo')
